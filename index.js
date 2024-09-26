223f90ef10f6ba41da01e9f83e3dd77c6fcbe9e6
rpatroni <rafael.patronilo@hotmail.com>
2024-09-26 15:10:52 +0100

    Switch to express

--- index.js
+++ index.js
@@ -1,43 +1,27 @@
 const sqlite3 = require('sqlite3');
-const http = require('http');
-const url = require('url');
-const path = require('path');
+const express = require('express');
 
 const db = new sqlite3.Database('database.sqlite');
 
-const server = http.createServer((req, res) => {
-  const {pathname} = url.parse(req.url);
-  if (pathname === '/posts') {
-    if (req.method === 'GET'){
-        
-        db.all('SELECT * FROM posts', 
-            (err, rows) => {
-                if (err) {
-                    res.writeHead(500);
-                    console.error(err);
-                    res.end();
-                    return;
-                } else {
-                    res.writeHead(200, {'Content-Type': 'application/json'});
-                    res.end(JSON.stringify(rows));
-                }
-            });
-    } else if (req.method === 'POST') {
-        let body = '';
-        req.on('data', (chunk) => {
-            body += chunk.toString();
-        });
-        req.on('end', () => {
-            const {title, content} = JSON.parse(body);
-            db.exec(`INSERT INTO posts (title, content) VALUES ("${title}", "${content}")`);
-            res.writeHead(201);
-            res.end();
-        });
-    }
-  } else {
-    res.writeHead(404);
-    res.end('Not found');
-  }
+const app = express();
+
+app.use(express.json());
+
+app.get("/posts", (req, res) => {
+    db.all('SELECT * FROM posts', (err, rows) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(rows);
+        }
+    });
 });
 
-server.listen(8080);
+app.post("/posts", (req, res) => {
+    let body = req.body;
+    const {title, content} = body;
+    db.exec(`INSERT INTO posts (title, content) VALUES ("${title}", "${content}")`);
+    res.sendStatus(201);
+});
+
+app.listen(8080);
